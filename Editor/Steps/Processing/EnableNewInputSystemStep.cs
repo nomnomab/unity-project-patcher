81eba0a99a773a15d52ee61862a7bbb0509272ac
Andrew Burke <nomnomab1@gmail.com>
2024-04-14 21:44:38 -0400

    New input system enabling step

---
+++ Editor/Steps/Processing/EnableNewInputSystemStep.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Nomnom.UnityProjectPatcher.Editor.Steps {
+    public readonly struct EnableNewInputSystemStep: IPatcherStep {
+        public UniTask<StepResult> Run() {
+            try {
+                var playerSettings = Resources.FindObjectsOfTypeAll<PlayerSettings>()[0];
+                var playerSettingsSo = new SerializedObject(playerSettings);
+                var activeInputHandlerProp = playerSettingsSo.FindProperty("activeInputHandler");
+                
+                // 0: Input Manager (Old)
+                // 1: Input System Package (New)
+                // 2: Both
+                activeInputHandlerProp.intValue = 1;
+                playerSettingsSo.ApplyModifiedPropertiesWithoutUndo();
+            } catch {
+                Debug.LogError("Could not find or modify PlayerSettings");
+                return UniTask.FromResult(StepResult.Failure);
+            }
+            
+            return UniTask.FromResult(StepResult.Success);
+        }
+    }
+}
